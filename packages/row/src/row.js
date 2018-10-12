6d79cb04c730171246335020519912c4c691d92b
zhoust <zhoust@guahao.com>
2018-10-12 23:16:24 +0800

    analysis el-row

--- packages/row/src/row.js
+++ packages/row/src/row.js
@@ -11,7 +11,9 @@ export default {
     },
     // row里面的col之间的距离
     gutter: Number,
+    // 将 `type` 属性赋值为 'flex'，可以启用 flex 布局
     type: String,
+    // 可通过 `justify` 属性来指定 start, center, end, space-between, space-around 其中的值来定义子元素的排版方式。
     justify: {
       type: String,
       default: 'start'
