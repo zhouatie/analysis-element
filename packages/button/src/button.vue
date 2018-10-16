fa20a3d3ec08d825d4d7eb3f7515df999a7a75b5
zhoust <zhoust@guahao.com>
2018-10-16 22:54:42 +0800

    analysis el-button

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,4 +1,11 @@
 <template>
+<!-- 
+  handleClick: 点击事件 
+  :disabled="buttonDisabled || loading"： buttonDisabled如下面mothods中；loading（父组件通过props传递过来）
+  autofocus：父组件通过props传入
+  type：button原生type，入button、submit
+  class: 通过class类名来控制按钮的样式、尺寸
+-->
   <button
     class="el-button"
     @click="handleClick"
@@ -17,6 +24,7 @@
       }
     ]"
   >
+  <!-- 父组件传递loading时，显示loading按钮 -->
     <i class="el-icon-loading" v-if="loading"></i>
     <i :class="icon" v-if="icon && !loading"></i>
     <span v-if="$slots.default"><slot></slot></span>
@@ -25,7 +33,8 @@
 <script>
   export default {
     name: 'ElButton',
-
+    // TODO: 分析到el-form的时候记得回来看
+    // 注入祖先元素provide的元素（这里inject注入的其实是el-form实例this
     inject: {
       elForm: {
         default: ''
@@ -64,6 +73,10 @@
       buttonSize() {
         return this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
       },
+      /**
+       * disabled: 用户传递的disabled
+       * this.elForm.diabled: 父组件el-form的disabled属性（通过inject注入）
+       *  */
       buttonDisabled() {
         return this.disabled || (this.elForm || {}).disabled;
       }
@@ -73,6 +86,10 @@
       handleClick(evt) {
         this.$emit('click', evt);
       }
+    },
+
+    created() {
+      console.log(this, 'this');
     }
   };
 </script>
